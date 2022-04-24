9c1f1f6ba246dbdc34756c2fac2c1b8a99072663
Stavan7 <kulkarnistavan25@gmail.com>
2022-04-24 15:53:19 +0530

    updated font and linear gradient

--- components/header.js
+++ components/header.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    StatusBar,
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView
 } from 'react-native';
-import Entypo from 'react-native-vector-icons/Entypo';
 import COLORS from '../constants/colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Header = ({ heading }) => {
-    return (
-        <View style={styles.header}>
-            <View>
-                <Text style={styles.headingText}>{heading}</Text>
-            </View>
-        </View>
-    );
+  return (
+    <SafeAreaView>
+      <LinearGradient
+        colors={['#233329', '#166d3b']}
+        start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+        style={styles.header}>
+        <Text style={styles.headingText}>{heading}</Text>
+      </LinearGradient>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        backgroundColor: COLORS.bottomTabBg,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20,
-        margin: 20,
-        ...Platform.select({
-            ios: {
-                height: 90,
-            },
-            android: {
-                height: 50,
-            },
-        }),
-    },
-    headingText: {
-        fontSize: 25,
-        color: 'white',
-        fontFamily: 'Roboto-Bold',
-    },
+  header: {
+    backgroundColor: COLORS.bottomTabBg,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 17,
+    margin: 20,
+    ...Platform.select({
+      ios: {
+        height: 90,
+      },
+      android: {
+        height: 50,
+      },
+    }),
+  },
+  headingText: {
+    fontSize: 23,
+    color: 'white',
+    fontFamily: 'Muli',
+  },
 });
 
 export default Header;
--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-    project: {
-        ios: {},
-        android: {},
-    },
-    assets: [
-        './assets/fonts/',
-        './assets/fonts/Roboto'
-    ],
-}
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/', './assets/fonts/Roboto', './assets/fonts/Muli'],
+};
