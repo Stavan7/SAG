2b4b3129008fd587e3eeb225a309966921670f4f
Stavan7 <kulkarnistavan25@gmail.com>
2023-09-03 16:45:17 +0530

    events detail page updated

--- screens/Events/EventsDetails.js
+++ screens/Events/EventsDetails.js
@@ -26,21 +26,23 @@ const EventsDetails = ({ route, navigation }) => {
     return (
         <SafeAreaView style={styles.view}>
             <View style={styles.container} >
-                <View style={styles.iconsContainer}>
-                    <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack()}>
-                        <FontAwesome
-                            name="close"
-                            color='white'
-                            size={moderateScale(25)}
-                        />
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity
+                    hitSlop={20}
+                    activeOpacity={0.5}
+                    style={styles.iconsContainer}
+                    onPress={() => navigation.goBack()} >
+                    <FontAwesome
+                        name="close"
+                        color='black'
+                        size={moderateScale(25)}
+                    />
+                </TouchableOpacity>
+
                 <Text style={styles.header}>{data.title}</Text>
                 <Text style={styles.date}>{data.date}</Text>
-            </View>
-
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{ uri: data.image }} />
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={{ uri: data.image }} />
+                </View>
             </View>
 
             <View style={styles.card}>
@@ -52,7 +54,7 @@ const EventsDetails = ({ route, navigation }) => {
                         tagsStyles={{
                             span: { fontFamily: FONTS.MEDIUM },
                             li: { fontFamily: FONTS.MEDIUM, marginBottom: 10 },
-                            body: { fontFamily: FONTS.MEDIUM, color: COLORS.WHITE, fontSize: 16 },
+                            body: { fontFamily: FONTS.MEDIUM, color: COLORS.BLACK, fontSize: 16 },
                         }}
                     />
                 </ScrollView>
@@ -65,61 +67,74 @@ const EventsDetails = ({ route, navigation }) => {
 const styles = ScaledSheet.create({
     view: {
         flex: 1,
-        backgroundColor: COLORS.WHITE
+        backgroundColor: '#EDF2EE'
     },
     container: {
         paddingBottom: '10@ms',
-        marginTop: '20@ms',
-        marginHorizontal: '10@ms',
+        zIndex: 1,
+        elevation: 5,
+        borderBottomLeftRadius: '25@ms',
+        borderBottomRightRadius: '25@ms',
+        backgroundColor: '#EDF2EE',
+        shadowOffset: {
+            width: 0,
+            height: '5@vs',
+        },
+        shadowOpacity: 0.57,
+        shadowRadius: 15.19,
+        paddingTop: '20@ms'
+        , paddingHorizontal: '10@ms'
     },
     iconsContainer: {
-        right: '-8@ms',
-        top: '-14@ms',
+        right: '10@ms',
+        top: '20@ms',
         width: '40@ms',
         height: '40@ms',
         position: 'absolute',
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: '50@ms',
-        backgroundColor: COLORS.BLACK,
+        backgroundColor: COLORS.GREEN,
     },
     imageContainer: {
         height: '210@ms',
         overflow: 'hidden',
-        marginHorizontal: "20@ms",
+        marginTop: '20@ms',
+        marginHorizontal: "10@ms",
     },
     image: {
         flex: 1,
         width: '100%',
-        borderRadius: '10@ms',
+        borderRadius: '25@ms',
         resizeMode: 'contain'
     },
     header: {
         marginTop: '10@ms',
         fontSize: '20@ms',
-        textAlign: 'center',
+        textAlign: 'left',
+        marginLeft: 10,
         color: COLORS.BLACK,
         fontFamily: FONTS.BOLD,
     },
     date: {
         fontSize: '17@ms',
-        textAlign: 'center',
+        textAlign: 'left',
+        marginLeft: 10,
         marginTop: '5@ms',
         color: COLORS.BLACK,
         fontFamily: FONTS.BOLD,
     },
     card: {
-        flex: 1,
         padding: 20,
-        marginTop: '10@ms',
-        borderTopRightRadius: '20@ms',
-        borderTopLeftRadius: '20@ms',
-        backgroundColor: COLORS.BLACK
+        marginTop: '-20@ms',
+        borderBottomLeftRadius: '25@vs',
+        borderBottomRightRadius: '25@vs',
+        backgroundColor: COLORS.GREEN
     },
     description: {
         textAlign: "left",
         fontSize: '16@ms',
-        color: COLORS.WHITE,
+        color: '#000',
         fontFamily: FONTS.REGULAR
     },
 })
